da5fe7b6e3d66cedc578a942d6c9cd59ef6ec87f
Irina Muimarova <mui.irisa@gmail.ru>
2019-12-17 13:28:13 +0700

    Initial commit

---
+++ src/main/java/servlets/MultServlet.java
@@ -0,0 +1,29 @@
+package servlets;
+
+import util.PageGenerator;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MultServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int number = 0;
+        boolean noError = true;
+        try {
+            number = Integer.parseInt(req.getParameter("value")) * 2;
+        } catch (Exception e) {
+            noError = false;
+        }
+        Map<String, Object> pageVariables = new HashMap<>();
+        resp.setContentType("text/html;charset=utf-8");
+        pageVariables.put("value", number);
+        resp.setStatus(noError ? HttpServletResponse.SC_OK : HttpServletResponse.SC_BAD_REQUEST);
+        resp.getWriter().println(new PageGenerator().getPage("number.html", pageVariables));
+    }
+
+}
